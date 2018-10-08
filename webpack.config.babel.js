b0107862f5ac2b5f84f26d647d68b672870fef8d
bguzman <bguzmanrio91@gmail.com>
2018-10-09 00:29:33 +0200

    Initial commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,32 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ScriptExtHtmlWebpackPlugin from 'script-ext-html-webpack-plugin';
+export default {
+    entry: path.join(__dirname, 'app/index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js/,
+            exclude: /(node_modules)/,
+            use: [{
+                loader: 'babel-loader'
+            }]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Custom template',
+            template: path.join(__dirname, 'public/index.html')
+        }),
+        new ScriptExtHtmlWebpackPlugin({
+            defaultAttribute: 'defer'
+        })
+    ],
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
---
+++ webpack.config.prod.babel.js
@@ -0,0 +1,37 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+
+export default {
+    entry: path.join(__dirname, 'app/index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js/,
+            exclude: /(node_modules)/,
+            use: [{
+                loader: 'babel-loader'
+            }]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Custom template',
+            template: path.join(__dirname, 'public/index.html')
+        }),
+        new ScriptExtHtmlWebpackPlugin({
+            defaultAttribute: 'defer'
+        }),
+        new CopyWebpackPlugin([{ from: 'public'}])
+    ],
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
