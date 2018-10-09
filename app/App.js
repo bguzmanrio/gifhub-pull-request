6b549767f66135f1721763bef8e26765364e5f45
bguzmanrio <bguzmanrio91@gmail.com>
2018-10-09 11:42:14 +0200

    Removed dist files. Basic search and retrieval functionality

--- app/App.js
+++ app/App.js
@@ -1,8 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class App extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			gifUrl: null,
+			keyword: null,
+			copied: false
+		};
+
+		this.mdCodeRef = React.createRef();
+		this.handleGifRequest = this.handleGifRequest.bind(this);
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleMDCopy = this.handleMDCopy.bind(this);
+	}
+
+	handleGifRequest() {
+		const keyParam = 'api_key=VwV9rz5sgKf0uBViFwBlU9b8H3lmossH';
+		const params = this.state.keyword ? `tag=${this.state.keyword}&${keyParam}` : keyParam;
+		fetch(`https://api.giphy.com/v1/gifs/random?${params}`).then(res => res.json()).then(response => {
+			this.setState({
+				gifUrl: response.data.image_url
+			});
+		})
+	}
+
+	handleInputChange(e) {
+		this.setState({
+			keyword: e.target.value
+		});
+	}
+
+	handleMDCopy() {
+		this.mdCodeRef.current.select();
+		document.execCommand('copy');
+		this.setState({
+			copied: true
+		});
+	}
+
 	render() {
-		return (<h1>My React App</h1>)
+
+		return (
+			<div>
+				<h1>GIFPRS</h1>
+				<input type="text" onChange={this.handleInputChange}></input>
+				<button onClick={this.handleGifRequest}>Get the gif!</button>
+				{this.state.gifUrl && (
+					<Fragment>
+						<img src={this.state.gifUrl} />
+						<input ref={this.mdCodeRef} type="text" defaultValue={`![](${this.state.gifUrl}`} readOnly />
+						<button onClick={this.handleMDCopy}>{this.state.copied ? 'Copied!' : 'Copy'}</button>
+					</Fragment>
+				)}
+				
+			</div>
+		)
 	}
 }
 
