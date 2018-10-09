a2c9d8fc31aced607c2a83316f7f918273b49d47
bguzman <bguzmanrio91@gmail.com>
2018-10-09 20:57:33 +0200

    Fix indentation

--- app/App.js
+++ app/App.js
@@ -3,89 +3,89 @@ import React, { Component, Fragment } from 'react';
 const prTitleRegExp = /PR[\s-]\d*[:]*[\s]*/gmi;
 
 function getPRTitle() {
-	const title = document.querySelector('#pull_request_title') || {};
-	return title.value || '';
+  const title = document.querySelector('#pull_request_title') || {};
+  return title.value || '';
 }
 
 const parsePRTitle = prTitle => prTitle.replace(prTitleRegExp, '');
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			gifUrl: null,
-			keyword: null,
-			copied: false,
-			isLoaded: false
-		};
+  constructor() {
+    super();
+    this.state = {
+      gifUrl: null,
+      keyword: null,
+      copied: false,
+      isLoaded: false
+    };
 
-		this.mdCodeRef = React.createRef();
-		this.handleImageLoad = this.handleImageLoad.bind(this);
-		this.handleGifRequest = this.handleGifRequest.bind(this);
-		this.handleInputChange = this.handleInputChange.bind(this);
-		this.handleMDCopy = this.handleMDCopy.bind(this);
-	}
+    this.mdCodeRef = React.createRef();
+    this.handleImageLoad = this.handleImageLoad.bind(this);
+    this.handleGifRequest = this.handleGifRequest.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleMDCopy = this.handleMDCopy.bind(this);
+  }
 
-	componentDidMount() {
-		chrome.tabs.executeScript({
-				code: '(' + getPRTitle + ')();' //argument here is a string but function.toString() returns function's code
-		}, (results) => {
-				//Here we have just the innerHTML and not DOM structure
-				this.setState({
-					keyword: parsePRTitle(results[0])
-				}, this.handleGifRequest);
-		});
-	}
+  componentDidMount() {
+    chrome.tabs.executeScript({
+        code: '(' + getPRTitle + ')();' //argument here is a string but function.toString() returns function's code
+    }, (results) => {
+        //Here we have just the innerHTML and not DOM structure
+        this.setState({
+          keyword: parsePRTitle(results[0])
+        }, this.handleGifRequest);
+    });
+  }
 
-	handleGifRequest() {
-		const keyParam = 'api_key=VwV9rz5sgKf0uBViFwBlU9b8H3lmossH';
-		const params = this.state.keyword ? `tag=${this.state.keyword}&${keyParam}` : keyParam;
+  handleGifRequest() {
+    const keyParam = 'api_key=VwV9rz5sgKf0uBViFwBlU9b8H3lmossH';
+    const params = this.state.keyword ? `tag=${this.state.keyword}&${keyParam}` : keyParam;
 
-		this.setState({
-			isLoaded: false
-		}, () => {
-			fetch(`https://api.giphy.com/v1/gifs/random?${params}`).then(res => res.json()).then(response => {
-				this.setState({
-					gifUrl: response.data.images.downsized_large.url
-				});
-			})
-		})
-	}
+    this.setState({
+      isLoaded: false
+    }, () => {
+      fetch(`https://api.giphy.com/v1/gifs/random?${params}`).then(res => res.json()).then(response => {
+        this.setState({
+          gifUrl: response.data.images.downsized_large.url
+        });
+      })
+    })
+  }
 
-	handleImageLoad() {
-		this.setState({
-			isLoaded: true
-		})
-	}
+  handleImageLoad() {
+    this.setState({
+      isLoaded: true
+    })
+  }
 
-	handleInputChange(e) {
-		this.setState({
-			keyword: e.target.value
-		});
-	}
+  handleInputChange(e) {
+    this.setState({
+      keyword: e.target.value
+    });
+  }
 
-	handleMDCopy() {
-		this.mdCodeRef.current.select();
-		document.execCommand('copy');
-		this.setState({
-			copied: true
-		});
-	}
+  handleMDCopy() {
+    this.mdCodeRef.current.select();
+    document.execCommand('copy');
+    this.setState({
+      copied: true
+    });
+  }
 
-	render() {
-		const imgStyles = {
-			display: this.state.isLoaded ? 'block' : 'none'
-		};
+  render() {
+    const imgStyles = {
+      display: this.state.isLoaded ? 'block' : 'none'
+    };
 
-		return (
-			<div style={{minWidth: '250px'}}>
-				<h1>GIFPRS -- GIFs on demand!</h1>
-				<input type="text" onChange={this.handleInputChange} defaultValue={this.state.keyword}></input>
-				<button onClick={this.handleGifRequest}>Get the gif!</button>
-				{this.state.gifUrl && (
-					<Fragment>
-						{!this.state.isLoaded && (
-							<div>Loading GIF...</div>
+    return (
+      <div style={{minWidth: '250px'}}>
+        <h1>GIFPRS -- GIFs on demand!</h1>
+        <input type="text" onChange={this.handleInputChange} defaultValue={this.state.keyword}></input>
+        <button onClick={this.handleGifRequest}>Get the gif!</button>
+        {this.state.gifUrl && (
+          <Fragment>
+            {!this.state.isLoaded && (
+              <div>Loading GIF...</div>
             )}
             <div style={imgStyles}>
               <img src={this.state.gifUrl} onLoad={this.handleImageLoad}/>
@@ -94,12 +94,12 @@ class App extends Component {
                 <button onClick={this.handleMDCopy}>{this.state.copied ? 'Copied!' : 'Copy'}</button>
               </div>
             </div>
-					</Fragment>
-				)}
-				
-			</div>
-		)
-	}
+          </Fragment>
+        )}
+        
+      </div>
+    )
+  }
 }
 
 export default App;
--- app/index.js
+++ app/index.js
@@ -5,5 +5,5 @@ import App from './App';
 render(<App />, document.getElementById('app'));
 
 if (module.hot) {
-    module.hot.accept();
+  module.hot.accept();
 }
