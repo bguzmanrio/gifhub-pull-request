4ce70674718306f334654155db3e0dcbcfc788e9
bguzmanrio <bguzmanrio91@gmail.com>
2019-03-09 01:17:12 +0100

    Test branch

--- actions.js
+++ actions.js
@@ -2,6 +2,9 @@ export const WRITE_DOM = 'write_dom_md_code';
 export const ASK_FOR_BODY = 'has_pr_body';
 export const ASK_FOR_TITLE = 'ASK_FOR_TITLE';
 
+console.log('123');
+
+
 export default {
   WRITE_DOM,
   ASK_FOR_BODY,
