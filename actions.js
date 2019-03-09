9781aac262f600b6a882121ddd4887547d968ef4
bguzmanrio <bguzmanrio91@gmail.com>
2019-03-09 01:15:19 +0100

    New action to retrieve the title

--- actions.js
+++ actions.js
@@ -1,7 +1,9 @@
 export const WRITE_DOM = 'write_dom_md_code';
 export const ASK_FOR_BODY = 'has_pr_body';
+export const ASK_FOR_TITLE = 'ASK_FOR_TITLE';
 
 export default {
   WRITE_DOM,
-  ASK_FOR_BODY
+  ASK_FOR_BODY,
+  ASK_FOR_TITLE
 };
